351f436b9875a2302b15f0ffebbd2037fb9a4f42
Nico <ubncr@student.kit.edu>
2023-11-18 19:41:25 +0100

    removed FirstComponent inside DebugPanel

--- src/pages/DebugPanel.tsx
+++ src/pages/DebugPanel.tsx
@@ -1,50 +1,10 @@
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import { TreeView } from '@mui/x-tree-view/TreeView';
-import { TreeItem } from '@mui/x-tree-view/TreeItem';
 import TraceGrid from "../components/TraceGrid";
-import { listen } from "@tauri-apps/api/event";
-import { useEffect } from 'react';
 
 function DebugPanel() {
-    useEffect(() => {
-        const unsubscribe = listen<any>("rx-frame", (event) => {
-          // Log the received event data
-          console.log("Received rx-frame event:", event.payload);
-        });
-    
-        return () => {
-          unsubscribe.then((f) => f());
-        };
-      }, []); // Empty dependency array ensures this effect runs once when the component mounts
-
     return (
         <>
-            <FirstComponent/> 
             <TraceGrid />
         </>
     );
 };
 export default DebugPanel;
-
-
-function FirstComponent() {
-    return (
-        <TreeView
-            aria-label="file system navigator"
-            defaultCollapseIcon={<ExpandMoreIcon />}
-            defaultExpandIcon={<ChevronRightIcon />}
-            sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
-        >
-            <TreeItem nodeId="1" label="Applications">
-                <TreeItem nodeId="2" label="Calendar" />
-            </TreeItem>
-            <TreeItem nodeId="5" label="Documents">
-                <TreeItem nodeId="10" label="OSS" />
-                <TreeItem nodeId="6" label="MUI">
-                    <TreeItem nodeId="8" label="index.js" />
-                </TreeItem>
-            </TreeItem>
-        </TreeView>
-    );
-}
