4bb6d75e99f518d705640e396b2c0772a0bafc8d
Niklas Heneka <93838730+NiklasHeneka@users.noreply.github.com>
2023-12-03 18:54:59 +0100

    First draft for setting the value of Object Entries.

--- src/pages/ObjectEntryPanel.tsx
+++ src/pages/ObjectEntryPanel.tsx
@@ -1,12 +1,36 @@
-import { NodeInformation } from "../types/NodeInformation";
+import {NodeInformation} from "../types/NodeInformation";
+import {useEffect, useState} from "react";
+import {ObjectEntryInformation} from "../types/ObjectEntryInformation.ts";
+import {invoke} from "@tauri-apps/api";
 
 interface ObjectEntryPanelProps {
-  node : NodeInformation,
-  name : string,
+    node: NodeInformation,
+    name: string,
 }
 
-function ObjectEntryPanel({node, name} : ObjectEntryPanelProps) {
-  return <h1> Hello {name} of {node.name} </h1>
+function ObjectEntryPanel({node, name}: ObjectEntryPanelProps) {
+    const [objectEntry, setObjectEntry] = useState<ObjectEntryInformation>({name: "", id: -1});
+
+    async function asyncFetchNetworkInfo() {
+
+        let objectEntryInformation = await invoke<ObjectEntryInformation>("object_entry_information", {
+            nodeName: node.name,
+            objectEntryName: name
+        });
+        setObjectEntry(objectEntryInformation);
+    }
+
+    useEffect(() => {
+        asyncFetchNetworkInfo().catch(console.error);
+    });
+
+
+    return <>
+        <h1> Hello {objectEntry.name} of {node.name} </h1>
+        <label>
+            Value: <input name="SetObjectDictionaryValue"/>
+        </label>
+    </>
 }
 
 export default ObjectEntryPanel;
