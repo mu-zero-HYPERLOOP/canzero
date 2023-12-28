62d921cd1f362897247d96f18bd60029d1543546
Niklas Heneka <93838730+NiklasHeneka@users.noreply.github.com>
2023-12-28 17:40:17 +0100

    Add 3d model of a car.

--- src/components/ControlGrid.tsx
+++ src/components/ControlGrid.tsx
@@ -1,18 +1,38 @@
 import PodTemperatures from './PodTemperatures';
 import { Grid, Paper, Skeleton, styled } from '@mui/material';
+import BMW from '../assets/bmw_m4.glb?url';
+import {PresentationControls, Stage, useGLTF} from '@react-three/drei';
+import {Canvas} from "@react-three/fiber";
+import { JSX } from 'react/jsx-runtime';
 
-const Item = styled(Paper)(({ theme }) => ({
+const Item = styled(Paper)(({theme}) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
     ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: 'center',
     color: theme.palette.text.secondary,
-  }));
+}));
 
 interface ControlGridProps {
     connectionSuccess: boolean;
 }
 
+function Model(props: JSX.IntrinsicAttributes & { [properties: string]: any; }) {
+    const { scene } = useGLTF(BMW);
+    return <primitive object={scene} scale={0.01} {...props} />;
+}
+
+function Pod3DModel() {
+    return (
+        <Canvas dpr={[1, 2]} shadows camera={{ fov: 45 }}>
+            <PresentationControls global speed={1.5} zoom={0.5} polar={[-Math.PI, Math.PI]}>
+                <Stage environment={null} shadows={true}>
+                    <Model scale={0.01}/>
+                </Stage>
+            </PresentationControls>
+        </Canvas>
+    );
+}
 
 function ControlGrid({ connectionSuccess }: ControlGridProps) {
     return (
@@ -30,6 +50,7 @@ function ControlGrid({ connectionSuccess }: ControlGridProps) {
                     <Grid item xs={6} md={8}>
                         <Item sx={{ width: "100%", height: "300px" }}>
                             3D Pod Model
+                            <Pod3DModel/>
                         </Item>
                     </Grid>
                     <Grid item xs={6} md={4}>
