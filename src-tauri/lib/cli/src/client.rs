75b19f0dc489320494df4c41520efc2c54a31d5a
kistenklaus <karlsasssie@gmail.com>
2024-05-21 18:51:07 +0200

    fix build_resources

--- src-tauri/lib/cli/src/client.rs
+++ src-tauri/lib/cli/src/client.rs
@@ -18,8 +18,7 @@ pub async fn command_client() -> Result<()> {
                 canzero_tcp::tcpcan::TcpCan::connect(std::net::SocketAddr::new(
                     connection.server_addr,
                     connection.service_port,
-                ), ConnectionId::None)
-                .await?,
+                ), ConnectionId::Client { request_id: false, sync_history: false }).await?
             );
             color_print::cprintln!("<green>Established connection to {} at {}:{}</green>", connection.server_name, connection.server_addr, connection.service_port);
 
