91dfd1a216cf71be729216f0f7a978588119fe80
Niklas Heneka <93838730+NiklasHeneka@users.noreply.github.com>
2024-06-13 14:49:02 +0200

    Add squares to guidance page.

--- src/visualizations/guidance/GuidanceVis.tsx
+++ src/visualizations/guidance/GuidanceVis.tsx
@@ -1,14 +1,43 @@
 import "./GuidanceVis.css"
-import {Stack, Typography} from "@mui/material";
+import {Box, List, ListItem, Stack, Typography} from "@mui/material";
+import ListItemText from "@mui/material/ListItemText";
 
 function GuidanceVis() {
+
     return (
         <Stack>
-            <Typography textAlign="center" paddingTop="0.5em" paddingBottom="0.5em" variant="h5">
-                TODO
+            <Typography textAlign="center" paddingTop="0.5em" variant="h5">
+                Guidance Position
             </Typography>
-            <div className="squareCool">
-            </div>
+            <Stack direction="row" spacing={2} sx={{
+                justifyContent: "space-evenly",
+                marginRight : 2,
+                marginLeft : 2,
+            }}>
+                <Box paddingTop="2.5em"><g id="guidance_vis">
+                    <div className="outerSquare">
+                    </div>
+                    <div className="innerSquare">
+                    </div>
+                </g>
+                </Box>
+                <Typography textAlign="center" paddingTop="0.5em" variant="h5">
+                    <List>
+                        <ListItem>
+                            <ListItemText primary="Distance from top: 5mm"/>
+                        </ListItem>
+                        <ListItem>
+                            <ListItemText primary="Distance from left: 5mm"/>
+                        </ListItem>
+                        <ListItem>
+                            <ListItemText primary="Distance from right: 5mm"/>
+                        </ListItem>
+                        <ListItem>
+                            <ListItemText primary="Distance from bottom: 5mm"/>
+                        </ListItem>
+                    </List>
+                </Typography>
+            </Stack>
         </Stack>
     )
 }
