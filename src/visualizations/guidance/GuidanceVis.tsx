1666d2a7b77a4b224901ce8bb4f72114a616f652
Niklas Heneka <93838730+NiklasHeneka@users.noreply.github.com>
2024-06-13 13:31:30 +0200

    Add guidance layout.

---
+++ src/visualizations/guidance/GuidanceVis.tsx
@@ -0,0 +1,17 @@
+import "./GuidanceVis.css"
+import {Stack, Typography} from "@mui/material";
+
+function GuidanceVis() {
+    return (
+        <Stack>
+            <Typography textAlign="center" paddingTop="0.5em" paddingBottom="0.5em" variant="h5">
+                TODO
+            </Typography>
+            <div className="squareCool">
+            </div>
+        </Stack>
+    )
+}
+
+
+export default GuidanceVis;
