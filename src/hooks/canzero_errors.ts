cfba9811a8be66add9db092112647c87cf376029
kistenklaus <karlsasssie@gmail.com>
2024-07-06 22:44:58 +0200

    fix build

--- src/hooks/canzero_errors.ts
+++ src/hooks/canzero_errors.ts
@@ -1,7 +1,7 @@
 import { invoke } from "@tauri-apps/api";
 import { listen } from "@tauri-apps/api/event";
 import { useEffect, useState } from "react";
-import ErrorEvent from "../object_entry/types/events/ErrorEvent";
+import { ErrorEvent } from "../object_entry/types/events/ErrorEvent";
 
 
 
